fc0da54f5c9d141778ec27d213981301b61e5586
mattpolski <mattpolski@gmail.com>
2018-03-15 14:03:43 +0100

    first test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,32 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+    System.out.println("Calculator's test:");
+
+    Calculator simpleCalculator = new Calculator();
+
+    int sum = simpleCalculator.add(4, 7);
+
+    if(sum == 11) {
+        System.out.println("Calculator works correctly");
+    } else {
+        System.out.println("Something is wrong");
     }
+}
+
 }
