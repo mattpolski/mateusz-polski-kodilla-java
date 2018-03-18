2cc30c5ff95f9eae1f10bbf451cb55e02f4ff601
mattpolski <mattpolski@gmail.com>
2018-03-18 17:36:54 +0100

    6.3

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,8 +1,7 @@
 package com.kodilla.testing.collection;
 import com.kodilla.testing.collection.OddNumbersExterminator;
 import org.junit.*;
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class CollectionTestSuite {
 
@@ -17,11 +16,30 @@ public class CollectionTestSuite {
 
     @Test
     public void testOddNumbersExterminatorEmptyList(){
-
+        ArrayList emptyList = new ArrayList();
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+            oddNumbersExterminator.exterminate(emptyList);
+        ArrayList result = oddNumbersExterminator.exterminate(emptyList);
+        System.out.println("Testing while list is empty");
+        Assert.assertEquals(0, result.size());
     }
 
     @Test
     public void testOddNumbersExterminatorNormalList(){
+        ArrayList normalList = new ArrayList();
+
+        normalList.add(2);
+        normalList.add(3);
+        normalList.add(6);
+        normalList.add(7);
+        normalList.add(8);
+        normalList.add(9);
+
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+            oddNumbersExterminator.exterminate(normalList);
+        ArrayList result = oddNumbersExterminator.exterminate(normalList);
+        System.out.println("Testing normal list");
+        Assert.assertEquals(3, result.size());
 
     }
 
