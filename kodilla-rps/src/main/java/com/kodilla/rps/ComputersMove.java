c5dcc01ac454ad5ad64d15ad9e1933c84bd2fe64
mattpolski <mattpolski@gmail.com>
2018-05-11 15:34:28 +0200

    7.7

--- kodilla-rps/src/main/java/com/kodilla/rps/ComputersMove.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/ComputersMove.java
@@ -8,11 +8,7 @@ public class ComputersMove {
 
     public Moves aiMove() {
         List<Moves> availableMoves = Arrays.asList(Moves.PAPER, Moves.ROCK, Moves.SCISSORS);
-        Random random = new Random();
-        for (int i = 0; i < availableMoves.size(); i++) {
-            int randomIndex = random.nextInt(availableMoves.size());
-            Moves computerMove = availableMoves.get(randomIndex);
-        }
-        return availableMoves.get(availableMoves.size());
+        Moves computerMove = availableMoves.get(new Random().nextInt(availableMoves.size()));
+        return computerMove;
     }
 }
