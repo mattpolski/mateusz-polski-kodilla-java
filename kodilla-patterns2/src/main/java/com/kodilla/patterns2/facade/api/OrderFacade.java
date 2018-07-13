04f008a08f6397488602aa9190511bf879e5402a
mattpolski <mattpolski@gmail.com>
2018-07-13 19:18:56 +0200

    12.6

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -0,0 +1,59 @@
+package com.kodilla.patterns2.facade.api;
+
+import com.kodilla.patterns2.facade.ShopService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class OrderFacade {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacade.class);
+    @Autowired
+    private ShopService shopService;
+
+    public void processOrder(final OrderDto order, final Long userId) throws OrderProcessingException {
+        final long orderId = shopService.openOrder(userId);
+        boolean wasError = false;
+        LOGGER.info("Registering new order, ID: " + orderId);
+        if (orderId < 0) {
+            LOGGER.error(OrderProcessingException.ERR_NOT_AUTHORISED);
+            wasError = true;
+            throw new OrderProcessingException(OrderProcessingException.ERR_NOT_AUTHORISED);
+        }
+        try {
+            for (ItemDto item : order.getItems()) {
+                LOGGER.info("Adding items " + item.getProductId() + ", " + item.getQuantity() + " pcs");
+                shopService.addItem(orderId, item.getProductId(), item.getQuantity());
+            }
+            final BigDecimal value = shopService.calculateValue(orderId);
+            LOGGER.info("Order value is: " + value + " USD");
+            if (!shopService.doPayment(orderId)) {
+                LOGGER.error(OrderProcessingException.ERR_PAYMENT_REJECTED);
+                wasError = true;
+                throw new OrderProcessingException(OrderProcessingException.ERR_PAYMENT_REJECTED);
+
+            }
+            LOGGER.info("Payment for order was done");
+            if (!shopService.verifyOrder(orderId)) {
+                LOGGER.error(OrderProcessingException.ERR_VERIFICATION_ERROR);
+                wasError = true;
+                throw new OrderProcessingException(OrderProcessingException.ERR_VERIFICATION_ERROR);
+            }
+            LOGGER.info("Order is ready to submit");
+            if (!shopService.submitOrder(orderId)) {
+                LOGGER.error(OrderProcessingException.ERR_SUBMITTING_ERROR);
+                wasError = true;
+                throw new OrderProcessingException(OrderProcessingException.ERR_SUBMITTING_ERROR);
+            }
+            LOGGER.info(String.format("Order %d submitted", orderId));
+        } finally {
+            if (wasError) {
+                LOGGER.info("Cancelling order " + orderId);
+                shopService.cancelOrder(orderId);
+            }
+        }
+    }
+}
