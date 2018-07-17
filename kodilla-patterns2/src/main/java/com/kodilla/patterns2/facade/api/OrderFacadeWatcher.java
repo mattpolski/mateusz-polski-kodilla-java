0da17ff472b0092d0c374bf6af6cf387b23c233f
mattpolski <mattpolski@gmail.com>
2018-07-17 20:49:17 +0200

    20.3

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeWatcher.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors;
+
+@Aspect
+@Component
+public class OrderFacadeWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+            "&& args(order, userId) && target(object)")
+    public void logEvent(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + " is processing order which contains items: " + order.getItems().stream()
+                .map(itemDto -> itemDto.getProductId().toString())
+                .collect(Collectors.toList()) + ", for user with Id: " + userId);
+    }
+}
